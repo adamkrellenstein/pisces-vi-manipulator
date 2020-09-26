fa739ac79d56d3e3107ca6b0591b8c3982e4b97e
adamkrellenstein <adam@krellenstein.com>
2020-09-26 01:00:02 -0400

    PWM + I2C Working!

--- manipulator.py
+++ manipulator.py
@@ -1,8 +1,25 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python2
+
+import time
 from inputs import get_gamepad
+
+from numpy import interp
+
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+kit.servo[0].set_pulse_width_range(0, 19988);
+
+
+
 print("***All Systems Nominal***")
+
+# kit.servo[0].angle = i
 
 while True:
     events = get_gamepad()
     for event in events:
-        print(event.ev_type, event.code, event.state)
+        if event.code == 'ABS_RY':
+            num = event.state
+            foo = interp(num,[0,32768],[0,180])
+            print(foo)
+        # print(event.ev_type, event.code, event.state)
